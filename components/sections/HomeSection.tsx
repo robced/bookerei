fdacba3f3abc31b80c59eab6dbcf27831602d926
robced <117061812+robced@users.noreply.github.com>
2023-01-03 13:41:22 -0800

    feat: partially complete home

--- components/sections/HomeSection.tsx
+++ components/sections/HomeSection.tsx
@@ -1,23 +1,39 @@
 import React from "react";
-import { Herobanner } from "../../interfaces/Herobanner.interface";
+import { HomeSection } from "../../interfaces/HomeSection.interface";
+import BookCard from "../elements/BookCard";
+import ServiceCard from "../elements/ServiceCard";
 import { PrimaryShape } from "../elements/Shapes";
 
-const HomeSection = ({ data }: Herobanner) => {
+const HomeSection = ({ products, services }: HomeSection) => {
   return (
-    <div className="h-full w-full relative overflow-hidden">
+    <div className="h-full w-full  relative overflow-hidden px-[200px] flex items-center justify-center">
       <div className="absolute right-[-15px] z-0">
         <PrimaryShape />
       </div>
-      <div className="w-full h-full z-20 relative flex">
-        <div className="w-1/2 h-full z-10 flex items-center justify-center flex-col">
-          <h2 className="">Find your next great read here.</h2>
-          <p className="">
-            Lorem ipsum dolor sit amet, consec-tetur adipiscing elit, sed do
+      <div className="w-full h-full z-20 relative flex items-center justify-between ">
+        <div className="w-1/2  h-full z-10 flex justify-center flex-col gap-5">
+          <h2 className="w-2/3 font-bold text-8xl font-robcon text-secondary">
+            Find your next great read here.
+          </h2>
+          <p className="font-roboto text-2xl w-2/3 text-secondary">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore.
           </p>
-          <button className="">Shop Now</button>
+          <div className="w-2/3 flex mt-4">
+            <button className="h-12 w-full bg-primary/40 font-roboto text-2xl font-bold mr-20 rounded-[12px] text-secondary">
+              Shop Now
+            </button>
+          </div>
+          <div className="w-2/3 flex gap-4">
+            <ServiceCard service={services[0]} />
+            <ServiceCard service={services[1]} />
+            <ServiceCard service={services[2]} />
+          </div>
         </div>
-        <div className="w-1/2 z-10">s</div>
+        <div className="w-1/2 h-full flex items-center justify-center z-10 gap-5 relative">
+          <BookCard products={products[0]} main />
+          <BookCard products={products[1]} />
+        </div>
       </div>
     </div>
   );
