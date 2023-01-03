a65175a781eee5bfe332677240c020ddef9f3933
robced <117061812+robced@users.noreply.github.com>
2023-01-02 16:49:53 -0800

    feat: setup context and layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { StateContext } from "../context/StateContext";
+import Layout from "../components/layout/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
