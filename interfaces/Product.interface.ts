698bd7472d138db2d88889c5e61a000392771d9f
robced <117061812+robced@users.noreply.github.com>
2023-01-03 13:40:50 -0800

    feat: add interfaces

---
+++ interfaces/Product.interface.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  _id: String;
+  name: String;
+  author: String;
+  slug: String;
+  image: any;
+  product: String;
+  price: String;
+  genre: String;
+  quote: String;
+  details: String;
+}
---
+++ interfaces/ServiceCard.interface.ts
@@ -0,0 +1,5 @@
+import { Service } from "./Service.interface";
+
+export interface ServiceCard {
+  service: Service;
+}
