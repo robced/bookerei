9fb4d427ff3df8ecd94ef83e80f9175bc04f15ab
robced <117061812+robced@users.noreply.github.com>
2023-01-02 16:48:11 -0800

    feat: add schemas

---
+++ sanity/schemas/product.ts
@@ -0,0 +1,55 @@
+export default {
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'array',
+      of: [{type: 'image'}],
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'author',
+      title: 'Author',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 90,
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    },
+    {
+      name: 'genre',
+      title: 'Genre',
+      type: 'string',
+    },
+    {
+      name: 'quote',
+      title: 'Quote',
+      type: 'string',
+    },
+    {
+      name: 'details',
+      title: 'Details',
+      type: 'string',
+    },
+  ],
+}
