ed035bfa5955fab1dec1ac276fbd2dda639621af
robced <117061812+robced@users.noreply.github.com>
2023-01-02 16:51:38 -0800

    feat: add shapes

---
+++ components/elements/Shapes.tsx
@@ -0,0 +1,15 @@
+export const PrimaryShape = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-screen w-[749px] z-0"
+      fill="none"
+      viewBox="0 0 749 1015"
+    >
+      <path
+        fill="#EECABF"
+        d="M0 15C0 6.716 6.716 0 15 0h721.507c8.284 0 15 6.716 15 15v1000l-308.91-52.34a99.904 99.904 0 01-19.374-5.33l-189.538-73.322a100.002 100.002 0 01-57.399-57.742l-68.001-178.947L7.893 409.863A100 100 0 010 370.922V15z"
+      ></path>
+    </svg>
+  );
+};
