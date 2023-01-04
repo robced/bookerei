2346c1251d8b90c2c1d28f991ce070ecbec3e975
robced <117061812+robced@users.noreply.github.com>
2023-01-04 11:05:51 -0800

    fix: change any to types

--- interfaces/AppContext.interface.ts
+++ interfaces/AppContext.interface.ts
@@ -1,8 +1,27 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, PropsWithChildren, SetStateAction } from "react";
+import { Product } from "./Product.interface";
+import { Service } from "./Service.interface";
 
 export interface AppContext {
   showCart: Boolean;
-  setShowCart: any;
+  setShowCart: Dispatch<SetStateAction<boolean>>;
   loading: Boolean;
-  setLoading: any;
+  setLoading: Dispatch<SetStateAction<boolean>>;
+  servicesData: Service[];
+  setServicesData: Dispatch<SetStateAction<Array<Service>>>;
+  productsData: Product[];
+  setProductsData: Dispatch<SetStateAction<Array<Product>>>;
+  searching: Boolean;
+  setSearching: Dispatch<SetStateAction<boolean>>;
+  filtering: Boolean;
+  setFiltering: Dispatch<SetStateAction<boolean>>;
+  searchPrompt: string;
+  setSearchPrompt: Dispatch<SetStateAction<string>>;
+  searchResults: Product[];
+  setSearchResults: Dispatch<SetStateAction<Array<Product>>>;
+}
+
+export interface AppContextProps extends PropsWithChildren {
+  products: Product[];
+  services: Service[];
 }
