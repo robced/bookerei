19406fbe8e9f21ff60f4c02147fac7039beb9ed3
robced <117061812+robced@users.noreply.github.com>
2023-01-04 11:08:36 -0800

    fix: misc

--- components/elements/BookCard.tsx
+++ components/elements/BookCard.tsx
@@ -4,31 +4,39 @@ import { BookCard } from "../../interfaces/BookCard.interface";
 import { urlFor } from "../../lib/client";
 import AddButton from "./AddButton";
 
-const BookCard = ({ products, main }: BookCard) => {
+const BookCard = ({ products, main, shop, color, text }: BookCard) => {
   const { name, author, price, image, details } = products;
-  console.log(name);
   return (
     <div
-      className={`w-[370px] h-[698px] bg-bg rounded-[50px] shadow-xl ${
+      className={`${
+        shop ? "w-[332.05px] h-[638px]" : "w-[370px] h-[698px]"
+      } bg-bg rounded-[50px] shadow-xl flex-shrink-0 ${
         name === "Outliers" ? "flex" : " hidden lg:flex"
       } flex-col relative items-center justify-evenly`}
     >
       <Image
         src={urlFor(image).url()}
         alt="Book Cover"
-        width={327}
-        height={494}
-        className="rounded-[50px] shadow-xl"
+        width={shop ? 287 : 327}
+        height={shop ? 454 : 494}
+        className="rounded-[40px] shadow-xl"
       ></Image>
       <div
-        className={`h-[165px] w-[337px] flex flex-col justify-evenly ${
-          main ? "bg-primary" : "bg-primary/40"
-        } rounded-[40px] p-5 font-roboto ${main ? "text-bg" : "text-primary"}`}
+        className={`${
+          shop ? "h-[165px] w-[297px]" : "h-[165px] w-[337px]"
+        } flex flex-col justify-evenly ${color} ${text} ${
+          shop ? "rounded-b-[35px]" : "rounded-b-[45px]"
+        } rounded-t-[30px] p-5 font-roboto 
+        }`}
       >
-        <h4 className={`font-bold  text-4xl`}>{name}</h4>
-        <p className="text-xl">{author}</p>
+        <h4 className={`font-bold  ${shop ? "text-3xl" : "text-4xl"}`}>
+          {name}
+        </h4>
+        <p className={`${shop ? "text-lg" : "text-xl"}`}>{author}</p>
         <div className="h-1/3 mt-3 flex justify-between items-center">
-          <p className="font-bold text-4xl">${price}</p>
+          <p className={`font-bold ${shop ? "text-3xl" : "text-4xl"}`}>
+            ${price}
+          </p>
           <AddButton filled={main ? true : false} />
         </div>
       </div>
