fccce09bf59d7eabe9575edae270e5d396d52418
robced <117061812+robced@users.noreply.github.com>
2023-01-04 11:06:03 -0800

    feat: add search context

--- context/StateContext.tsx
+++ context/StateContext.tsx
@@ -1,11 +1,25 @@
 import { createContext, PropsWithChildren, useContext, useState } from "react";
-import { AppContext } from "../interfaces/AppContext.interface";
+import {
+  AppContext,
+  AppContextProps,
+} from "../interfaces/AppContext.interface";
+import { client } from "../lib/client";
 
 const Context = createContext({} as AppContext);
 
-export const StateContext = ({ children }: PropsWithChildren) => {
-  const [loading, setLoading] = useState(false);
+export const StateContext = ({ children }: AppContextProps) => {
+  const [loading, setLoading] = useState(true);
+  const [servicesData, setServicesData] = useState<any>([]);
+  const [productsData, setProductsData] = useState<any>([]);
+  const [searching, setSearching] = useState(false);
+  const [searchPrompt, setSearchPrompt] = useState("");
+  const [searchResults, setSearchResults] = useState<any>([]);
+  const [filtering, setFiltering] = useState(false);
   const [showCart, setShowCart] = useState(false);
+
+  if (servicesData.length > 2 && productsData.length > 2) {
+    setLoading(false);
+  }
 
   return (
     <Context.Provider
@@ -14,6 +28,18 @@ export const StateContext = ({ children }: PropsWithChildren) => {
         setShowCart,
         loading,
         setLoading,
+        servicesData,
+        setServicesData,
+        productsData,
+        setProductsData,
+        searching,
+        setSearching,
+        filtering,
+        setFiltering,
+        searchPrompt,
+        setSearchPrompt,
+        searchResults,
+        setSearchResults,
       }}
     >
       {children}
