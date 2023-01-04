20c458e576c0df9051e8d4fb12de7d86ddc52342
robced <117061812+robced@users.noreply.github.com>
2023-01-04 11:36:17 -0800

    feat: add handle funcs

--- components/elements/Filter.tsx
+++ components/elements/Filter.tsx
@@ -1,8 +1,34 @@
 import React from "react";
-import BookCard from "./BookCard";
+import { useStateContext } from "../../context/StateContext";
 import { SearchIcon } from "./Icons";
 
 const Filter = () => {
+  const {
+    setFilteringAll,
+    setFilteringAuthors,
+    setFilteringGenre,
+    filteringAll,
+    filteringAuthors,
+    filteringGenre,
+  } = useStateContext();
+
+  const handleFilteringAll = () => {
+    setFilteringAll(true);
+    setFilteringAuthors(false);
+    setFilteringGenre(false);
+  };
+
+  const handleFilteringAuthors = () => {
+    setFilteringAll(false);
+    setFilteringAuthors(true);
+    setFilteringGenre(false);
+  };
+
+  const handleFilteringGenre = () => {
+    setFilteringAll(false);
+    setFilteringAuthors(false);
+    setFilteringGenre(true);
+  };
   // TODO: BOOK FILTER
   return (
     <div className="h-14 w-full bg-secbase rounded-2xl flex items-center justify-between">
@@ -14,10 +40,37 @@ const Filter = () => {
           placeholder="Search..."
         />
       </div>
-      <div className="w-1/3 flex items-center justify-evenly font-roboto text-xl">
-        <button className="text-secondary">All</button>
-        <button className="text-secondary">Genre</button>
-        <button className="text-secondary">Authors</button>
+      <div className="w-1/3 flex items-center justify-evenly font-roboto text-xl font-medium">
+        <button
+          onClick={handleFilteringAll}
+          className={` h-8 w-fit px-3 rounded-lg ${
+            filteringAll
+              ? "bg-secondary/50 text-bg"
+              : "bg-transparent text-secondary"
+          }`}
+        >
+          All
+        </button>
+        <button
+          onClick={handleFilteringGenre}
+          className={` h-8 w-fit px-3 rounded-lg ${
+            filteringGenre
+              ? "bg-secondary/50 text-bg"
+              : "bg-transparent text-secondary"
+          }`}
+        >
+          Genre
+        </button>
+        <button
+          onClick={handleFilteringAuthors}
+          className={` h-8 w-fit px-3 rounded-lg ${
+            filteringAuthors
+              ? "bg-secondary/50 text-bg"
+              : "bg-transparent text-secondary"
+          }`}
+        >
+          Authors
+        </button>
       </div>
     </div>
   );
