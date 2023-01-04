bcb54b7b3937b7428a078b77a3db4faca7c1cb22
robced <117061812+robced@users.noreply.github.com>
2023-01-04 11:08:47 -0800

    fix: add button options

--- components/elements/AddButton.tsx
+++ components/elements/AddButton.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Button } from "../../interfaces/Button.interface";
 import { AddIcon } from "./Icons";
 
-const AddButton = ({ filled }: Button) => {
+const AddButton = ({ filled, shop }: Button) => {
   return (
     <button
-      className={`h-12 w-12 flex items-center justify-center rounded-[15px] ${
+      className={`${
+        shop ? "h-10 w-10" : "h-12 w-12"
+      } flex items-center justify-center rounded-[15px] ${
         filled ? "bg-[#EECABF]" : "bg-primary"
       } ${filled ? "text-primary" : "text-[#EECABF]"}`}
     >
